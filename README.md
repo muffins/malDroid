537b6e710aded1a3ec2c401669c7f2705676390f
PoppySeedPlehzr <nanderson7@gmail.com>
2014-09-29 11:32:05 -0400

    Updated GUI, added file upload.
    Added webpages for About, Contact, and Source.  Also added an upload
    feature on the main webpage, which will take .txt, .png, .jpg, .gif, and
    upload it to the uploads/apk_samples/ folder.  Currently if the file
    already exists it will be overwritten, so... we'll need to add
    functionality to detect duplicate filenames.
    
    Also updated the README to contain 'TODO' for simple milestones
    we need to get done

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ malDroid
 ========
 
 A web application for analysing potentially malicious Android applications.
+
+### TODO:
+ * Design application reports, these should be inserted
+   into the DB, and query-able via a hash of the APK
+
+ * Design the view for reports :P
+
+ * Create a list of APK scanners/analysis engines/things we can use
+   to scan and generate our reports.
+   - Virus Total
+   - Andrubis
+   - ...
+
+ * Hosting? Digital Ocean is cheap, we also need a domain name, and
+   may have to rename the project, as maldroid is a rock band :(
+
+ * Refine upload engine, so that only APKs are uploadable.  Right now
+   a few files are uploadable for testing purposes :P
--- maldroid.py
+++ maldroid.py
@@ -2,18 +2,21 @@
 import os
 import sqlite3
 from time import gmtime, strftime
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from werkzeug import secure_filename
 
+# Create and Configure the flask app
 app = Flask(__name__)
+UPLOAD_FOLDER = './uploads/apk_samples'
+ALLOWED_EXTENSIONS = set(['txt','png','jpg','jpeg','gif']) # Default upload ext set
 app.config.from_object(__name__)
-
 app.config.update(dict(
 	DATABASE = os.path.join(app.root_path, 'maldroid.db'),
 	DEBUG = True,
-	SECRET_KEY = 'd3v3lopm3nt_k3y',
-	USERNAME = 'admin',
-	PASSWORD = 'default'
+	UPLOAD_FOLDER = UPLOAD_FOLDER
+	#SECRET_KEY = 'd3v3lopm3nt_k3y',
+	#USERNAME = 'admin',
+	#PASSWORD = 'default'
 	))
 
 
@@ -24,22 +27,21 @@ def connect_db():
 	return rv
 
 
-""" Opens a brand new database connection if one doesn't presently exist for the current
-application context """
+# Open the DB connection if it doesn't already exist
 def get_db():
 	if not hasattr(g, 'sqlite_db'):
 		g.sqlite_db = connect_db()
 	return g.sqlite_db
 
 
-""" Closes the db again at the end of a request """
+# Close the DB connection at the end of requests.
 @app.teardown_appcontext
 def close_db(error):
 	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
 
 
-""" Initialize the Database """
+# Setup the database.
 def init_db():
 	with app.app_context():
 		db = get_db()
@@ -48,15 +50,64 @@ def init_db():
 		db.commit()
 
 
+# At some point, this needs to be refined, to ensure that the
+# file itself is appropriate to be on our server, as opposed to
+# having a proper file extension :P
+def allowed_file(fname):
+	return '.' in fname and fname.split('.')[-1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+	if request.method == 'POST':
+		file = request.files['file']
+		if file and allowed_file(file.filename):
+			fname = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+			return redirect(url_for('report', fname=fname))
+	return redirect(url_for('error'))
+
+
+#@app.route('/report', methods=['GET','POST'])
+@app.route('/report')
+def report():
+	apkname = request.args.get('fname')
+	return redirect(url_for('home'))
+
+
 @app.route('/')
-@app.route('/index.html')
-def show_entries():
-	db = get_db()
-	cur = db.execute('select title, text, timestamp from entries order by id desc')
-	entries = cur.fetchall()
-	return render_template('show_entries.html', entries=entries)
+@app.route('/home')
+def home():
+	#db = get_db()
+	#cur = db.execute('select title, text, timestamp from entries order by id desc')
+	#entries = cur.fetchall()
+	return render_template('index.html')
+
+# Route for Contact form
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
+# Route for About form
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+# Generic error page
+@app.route('/error')
+def error():
+	return render_template('error.html')
 
 
+
+# This is the default manner in which to uplpoad a file in flask.  Right
+# now I'm just following the tutorial here: http://flask.pocoo.org/docs/0.10/patterns/fileuploads/
+# but at some point we'll want to restrict this to very very very specifically
+# android APKs, so that our server isn't being flooded with files :S
+
+
+
+"""
 @app.route('/add', methods=['POST'])
 def add_entry():
 	current_time = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
@@ -68,8 +119,9 @@ def add_entry():
 	db.commit()
 	flash('New entry was successfully posted')
 	return redirect(url_for("show_entries"))
+"""
 
-
+"""
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	error = None
@@ -81,16 +133,20 @@ def login():
 			flash('You have been successfully logged in')
 			return redirect(url_for('show_entries'))
 	return render_template('login.html', error=error)
+"""
 
-
+"""
 @app.route('/logout')
 def logout():
 	session.pop('logged_in', None)
 	flash('You were logged out')
 	return redirect(url_for('show_entries'))
+"""
 
 
 if __name__ == '__main__':
+	
+
 	app.run()
 
 
