1959f6e4f7cd72648fe2b40d58c02d20d2707931
PoppySeedPlehzr <nanderson7@gmail.com>
2014-10-31 11:01:32 -0400

    Adding maldroid image

--- maldroid.py
+++ maldroid.py
@@ -14,9 +14,9 @@ app.config.update(dict(
 	DEBUG = True,
 	UPLOAD_FOLDER = UPLOAD_FOLDER,
 	MAX_CONTENT_LENGTH = 32 * 1024 * 1024 # Limit upload size to 30MB
-	#SECRET_KEY = 'd3v3lopm3nt_k3y',
-	#USERNAME = 'admin',
-	#PASSWORD = 'default'
+	#SECRET_KEY = '',,
+	#USERNAME = '',
+	#PASSWORD = ''
 	))
 
 
@@ -112,49 +112,7 @@ def error():
 # now I'm just following the tutorial here: http://flask.pocoo.org/docs/0.10/patterns/fileuploads/
 # but at some point we'll want to restrict this to very very very specifically
 # android APKs, so that our server isn't being flooded with files :S
-
-
-
-"""
-@app.route('/add', methods=['POST'])
-def add_entry():
-	current_time = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
-	if not session.get('logged_in'):
-		abort(401)
-	db = get_db()
-	db.execute('insert into entries (title, text, timestamp) values (?, ?, ?)',
-		[request.form['title'], request.form['text'], current_time])
-	db.commit()
-	flash('New entry was successfully posted')
-	return redirect(url_for("show_entries"))
-"""
-
-"""
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid Logon Credentials'
-		else:
-			session['logged_in'] = True
-			flash('You have been successfully logged in')
-			return redirect(url_for('show_entries'))
-	return render_template('login.html', error=error)
-"""
-
-"""
-@app.route('/logout')
-def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out')
-	return redirect(url_for('show_entries'))
-"""
-
-
 if __name__ == '__main__':
-	
-
 	app.run()
 
 
