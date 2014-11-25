c0c53645539e8a330bf0d70d51b9745a7b89d67e
PoppySeedPlehzr <nanderson7@gmail.com>
2014-11-25 00:49:40 -0500

    Updated upload size to 64 MB

--- maldroid/maldroid_conf.py
+++ maldroid/maldroid_conf.py
@@ -7,7 +7,7 @@ import os
     ---> ADDED APIKEY TO SUPPORT VirusTotal Module (virustotal.py)
 
 """
-MAX_UPLOAD_SIZE = 32 * 1024 * 1024 # Limit upload size to 30MB
+MAX_UPLOAD_SIZE = 64 * 1024 * 1024 # Limit upload size to 64MB
 UPLOAD_FOLDER   = os.path.join(os.getcwd(), 'uploads/apk_samples')
 SQLITE_DB       = "maldroid.db"
 APIKEY		= "1b5439e68266c59ffc4972a08dc77614cfe69440d9672c5cf77cb94cee7bac6d"  #Used to authenticate connections with VirusTotal
